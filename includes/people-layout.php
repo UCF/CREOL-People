bc80821b3ad63d00d95f22b456e854a526a86eac
Jonathan Connor <125600046+jconn0@users.noreply.github.com>
2024-06-13 18:09:11 +0000

    ..

--- includes/people-layout.php
+++ includes/people-layout.php
@@ -2,24 +2,48 @@
 
 function people_display() {
     $args = array(
-        'posts_per_page' => -1, 
-        'post_type' => 'person',   
-        'post_status' => 'publish',
+        'posts_per_page' => -1,
+        'post_type'      => 'person',
+        'post_status'    => 'publish',
         'category_name'  => 'core-faculty'
     );
 
     $posts = get_posts($args);
 
     if (!empty($posts)) {
+        // Include custom styles
+        echo '<style>
+            .custom-card {
+                border: none; 
+                border-radius: 10px;  
+            }
+            .custom-card img {
+                width: 100%; 
+                height: auto; 
+                border-top-left-radius: 10px;  
+                border-top-right-radius: 10px;
+            }
+            .custom-card .card-body {
+                padding: 10px;  
+            }
+            .custom-card .card-title a {
+                color: #333; 
+                text-decoration: none;  
+            }
+            .custom-card .card-title a:hover {
+                color: #555;
+            }
+        </style>';
+
         echo '<div class="row">';
         foreach ($posts as $post) {
             setup_postdata($post);
-            $featured_image = get_the_post_thumbnail($post->ID, 'thumbnail');
+            $featured_image = get_the_post_thumbnail($post->ID, 'full');
 
             echo '<div class="col-lg-2 col-md-3 col-sm-4 col-6">';
-            echo '<div class="card mb-4">'; 
+            echo '<div class="card custom-card">';
             if (!empty($featured_image)) {
-                echo '<img src="' . get_the_post_thumbnail_url($post->ID, 'thumbnail') . '" class="card-img-top" alt="' . get_the_title($post) . '">'; 
+                echo $featured_image;
             }
             echo '<div class="card-body">';
             echo '<h5 class="card-title"><a href="' . get_permalink($post) . '">' . get_the_title($post) . '</a></h5>';
@@ -27,7 +51,7 @@ function people_display() {
             echo '</div>'; 
             echo '</div>'; 
         }
-        echo '</div>'; 
+        echo '</div>';
         wp_reset_postdata();
     } else {
         echo 'No posts found.';
