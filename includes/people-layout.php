a384ac0d34a398a2af327b10557a4ab741794373
Jonathan Connor <125600046+jconn0@users.noreply.github.com>
2024-06-13 20:41:34 +0000

    ..

--- includes/people-layout.php
+++ includes/people-layout.php
@@ -21,7 +21,7 @@ function people_display() {
 
         $posts = get_posts($args);
 
-        echo "<h2 class='section-title auto-section mb-0' data-section-link-title='$title'>$title</h2>";
+        echo "<h2 class='section-title auto-section mb-10' data-section-link-title='$title'>$title</h2>";
 
         if (!empty($posts)) {
             echo '<style>
