5d8d85640de623e332af1578eb396cccff5c8867
Jonathan Connor <125600046+jconn0@users.noreply.github.com>
2024-06-13 17:59:14 +0000

    .

--- includes/people-layout.php
+++ includes/people-layout.php
@@ -11,20 +11,25 @@ function people_display() {
     $posts = get_posts($args);
 
     if (!empty($posts)) {
-        echo '<ul>';
+        // Use a div with row class for the grid
+        echo '<div class="row">';
         foreach ($posts as $post) {
             setup_postdata($post);
             $featured_image = get_the_post_thumbnail($post->ID, 'thumbnail');
 
-            echo '<li>';
+            echo '<div class="col-md-2">';
+            echo '<div class="card">';
             if (!empty($featured_image)) {
-                echo $featured_image; 
+                echo '<div class="card-img-top">' . $featured_image . '</div>'; 
             }
-            echo '<a href="' . get_permalink($post) . '">' . get_the_title($post) . '</a>';
-            echo '</li>';
+            echo '<div class="card-body">';
+            echo '<h5 class="card-title"><a href="' . get_permalink($post) . '">' . get_the_title($post) . '</a></h5>';
+            echo '</div>';
+            echo '</div>';
+            echo '</div>';
         }
-        echo '</ul>';
-        wp_reset_postdata(); 
+        echo '</div>'; // Close row
+        wp_reset_postdata();
     } else {
         echo 'No posts found.';
     }
