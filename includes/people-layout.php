c0ba2e4b26d6b8db5341b55e35c6f7df470ac862
Jonathan Connor <125600046+jconn0@users.noreply.github.com>
2024-06-13 17:47:51 +0000

    .

--- includes/people-layout.php
+++ includes/people-layout.php
@@ -2,16 +2,10 @@
 
 function people_display() {
     $args = array(
-        'posts_per_page' => -1,
-        'post_type'      => 'person',
-        'post_status'    => 'publish',
-        'tax_query'      => array(
-            array(
-                'taxonomy' => 'faculty_type',
-                'field'    => 'slug',
-                'terms'    => 'core-faculty'
-            )
-        )
+        'posts_per_page' => -1, 
+        'post_type' => 'person',   
+        'post_status' => 'publish',
+		'category_name'  => 'core-faculty'
     );
 
     $posts = get_posts($args);
@@ -20,17 +14,14 @@ function people_display() {
         echo '<ul>';
         foreach ($posts as $post) {
             setup_postdata($post);
-            $featured_image = get_the_post_thumbnail($post->ID, 'thumbnail'); 
-
-            echo '<li>';
-            if (!empty($featured_image)) {
-                echo $featured_image; 
+			$featured_image = get_the_post_thumbnail($post->ID, 'thumbnail');
+			if (!empty($featured_image)) {
+                echo $featured_image; // Display the featured image
             }
-            echo '<a href="' . get_permalink($post) . '">' . get_the_title($post) . '</a>';
-            echo '</li>';
+            echo '<li><a href="' . get_permalink($post) . '">' . get_the_title($post) . '</a></li>';
         }
         echo '</ul>';
-        wp_reset_postdata();
+        wp_reset_postdata(); 
     } else {
         echo 'No posts found.';
     }
