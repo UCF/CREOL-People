45aa878a859620229b46a0d8c5f2aea5170ecc03
Jonathan Connor <125600046+jconn0@users.noreply.github.com>
2024-06-13 20:05:48 +0000

    .

--- includes/people-layout.php
+++ includes/people-layout.php
@@ -44,8 +44,8 @@ function people_display() {
             .job-title {
                 font-size: 0.85rem;
                 color: #666;
-                margin-top: -5px;
-                margin-bottom: 5px;
+                margin-top: -1em;
+                margin-bottom: 0.5em;
                 display: block;
             }
         </style>';
